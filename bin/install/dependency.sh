8fe034177c8c2107d5b4770c803ea906a6fa4167
xl-xueling <better_xueling@126.com>
2023-08-11 11:31:53 +0800

    init

--- bin/install/dependency.sh
+++ bin/install/dependency.sh
@@ -26,7 +26,9 @@ function setComponentsEnv(){
         remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo export ${homename}=${dist_dir}/${service} >> /root/.bashrc"
         if [ "$service" == "hadoop" ];then
           remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo export HADOOP_CONF_DIR='$'HADOOP_HOME/etc/hadoop >> /home/${DEPLOY_USER}/.bashrc"
+          remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo export HADOOP_CLASSPATH=`hadoop classpath` >> /home/${DEPLOY_USER}/.bashrc"
           remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo export HADOOP_CONF_DIR='$'HADOOP_HOME/etc/hadoop >> /root/.bashrc"
+          remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo export HADOOP_CLASSPATH=`hadoop classpath` >> /root/.bashrc"
         fi
         remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "echo PATH='$'${homename}/bin:'$'PATH >> /root/.bashrc"
         remoteExecute ${CUR_DIR}/common/exec.exp ${CUR_USER} ${ip} ${NODES_MAP[${ip}]} "sed -i '/export PATH/d' /root/.bashrc"
